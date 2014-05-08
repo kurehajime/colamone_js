843b4873a9aa2d99a84ecf8557a91537ba7764a0
kurehajime <xiidec@gmail.com>
2014-05-08 23:27:36 +0900

    AI強化
    全滅勝利や全滅に近い勝利を判定。

--- ai.js
+++ ai.js
@@ -74,14 +74,49 @@ var PIECE_POINT={1:3500,
                  8:2400
                 }
 var ZOC_POINTS=[80,80,80,80,80,160];
-var EFF_POINTS=[80,80,80,80,80,160];
+var EFF_POINTS=[100,100,100,100,100,160];
 var score=0;
+var NEARWIN_POINT=6000;
 
 function copyMap(wkMap){
     var rtnMap=new Object();
-    for(var num in wkMap){
-        rtnMap[num]=wkMap[num];
-    }
+    //不格好だがループするより高速。
+    rtnMap[0]=wkMap[0];
+    rtnMap[10]=wkMap[10];
+    rtnMap[20]=wkMap[20];
+    rtnMap[30]=wkMap[30];
+    rtnMap[40]=wkMap[40];
+    rtnMap[50]=wkMap[50];
+    rtnMap[1]=wkMap[1];
+    rtnMap[11]=wkMap[11];
+    rtnMap[21]=wkMap[21];
+    rtnMap[31]=wkMap[31];
+    rtnMap[41]=wkMap[41];
+    rtnMap[51]=wkMap[51];
+    rtnMap[2]=wkMap[2];
+    rtnMap[12]=wkMap[12];
+    rtnMap[22]=wkMap[22];
+    rtnMap[32]=wkMap[32];
+    rtnMap[42]=wkMap[42];
+    rtnMap[52]=wkMap[52];
+    rtnMap[3]=wkMap[3];
+    rtnMap[13]=wkMap[13];
+    rtnMap[23]=wkMap[23];
+    rtnMap[33]=wkMap[33];
+    rtnMap[43]=wkMap[43];
+    rtnMap[53]=wkMap[53];
+    rtnMap[4]=wkMap[4];
+    rtnMap[14]=wkMap[14];
+    rtnMap[24]=wkMap[24];
+    rtnMap[34]=wkMap[34];
+    rtnMap[44]=wkMap[44];
+    rtnMap[54]=wkMap[54];
+    rtnMap[5]=wkMap[5];
+    rtnMap[15]=wkMap[15];
+    rtnMap[25]=wkMap[25];
+    rtnMap[35]=wkMap[35];
+    rtnMap[45]=wkMap[45];
+    rtnMap[55]=wkMap[55];
     return rtnMap;
 }
 
@@ -89,6 +124,7 @@ function copyMap(wkMap){
 //終局判定。Return:Bool
 function isEnd(turn_player,wkMap){
     var sum=0;
+    //点数勝利
     if(turn_player>0){       
         sum+=wkMap[0]+wkMap[10]+wkMap[20]
             +wkMap[30]+wkMap[40]+wkMap[50];
@@ -102,45 +138,83 @@ function isEnd(turn_player,wkMap){
             return true;
         }
     }
-    return false;
-}
+    //全滅勝利
+    sum=0;
+    if(turn_player>0){
+        for(num in wkMap){
+            if(wkMap[num]<0){
+                sum+=Math.abs(wkMap[num]);   
+            }
+        }
+    }else if(turn_player<0){
+        for(num in wkMap){
+            if(wkMap[num]>0){
+                sum+=Math.abs(wkMap[num]);   
+            }
+        }
+    }
+    if(sum===0){
+        return true;
+    }
+    return false;        
 
+}
+//ほぼ勝ち判定。Return:Bool
+function isEndNear(turn_player,wkMap){
+    var sum=0;
+    if(turn_player>0){
+        for(num in wkMap){
+            if(wkMap[num]<0){
+                sum+=Math.abs(wkMap[num]);   
+            }
+        }
+    }else if(turn_player<0){
+        for(num in wkMap){
+            if(wkMap[num]>0){
+                sum+=Math.abs(wkMap[num]);   
+            }
+        }
+    }
+    if(sum<8){
+        return true;
+    }else{
+        return false;        
+    }
+}
 //動かせるマスを返す。Return:[NN,NN,NN...]
 function getCanMovePanelX(panel_num,wkMap,ownflag){
     var number = wkMap[panel_num];
     var x = Math.floor(panel_num / 10);
     var y = Math.floor(panel_num % 10);
     var canMove=new Array;
-    if(number==0){
+    if(number===0){
         return canMove;   
     }
     //アガリのコマは動かしたらダメ。
-    if(number>0 && y==0){
+    if(number>0 && y===0){
         return canMove;   
-    }else if(number<0 && y==5){
+    }else if(number<0 && y===5){
         return canMove;   
     }
-    
-    for(var i=0;i<=PIECES[number].length-1;i++){
-        var target_x= x + Math.floor(i%3)-1;
-        var target_y= y + Math.floor(i/3)-1;
-        if(PIECES[number][i]==0){
+    for(var i=0;i<PIECES[number].length;i++){
+        if(PIECES[number][i]===0){
             continue;
         }
+        var target_x= x + Math.floor(i%3)-1;
+        var target_y= y + Math.floor(i/3)-1;
         if(target_x<0 || target_y<0||target_x>5||target_y>5 ){
             continue;
         }
-        var target_number=wkMap[target_x*10+target_y];
-        if(target_number*number>0 && ownflag==false){
+        var idx=target_x*10+target_y;
+        var target_number=wkMap[idx];
+        if(ownflag===false&&target_number*number>0){
             continue;   
         }
         //アガリのコマはとったらダメ。
-        if(target_number>0 && target_y==0){
+        if((target_number>0 && target_y===0)||(target_number<0 &&target_y===5)){
             continue;   
-        }else if(target_number<0 &&target_y==5){
-            continue;
         }
-        canMove.push(target_x*10+target_y);
+        canMove.push(idx);
     }
     return canMove;
 }
@@ -164,17 +238,51 @@ function getNodeMap(queue,wkMap,turn_player){
     return nodeList;
 }
 
-
-
-
 //ZOCを返す。 Return:Map{NN:[Blue,Red]}
 function getZOC(wkMap,turn_player){
     var zocMap=new Object();
-    for(i in whiteMap){
-        zocMap[i]=[0,0];
-    }
+    //不格好だがループするより高速。
+    zocMap[0]=[0,0];
+    zocMap[10]=[0,0];
+    zocMap[20]=[0,0];
+    zocMap[30]=[0,0];
+    zocMap[40]=[0,0];
+    zocMap[50]=[0,0];
+    zocMap[1]=[0,0];
+    zocMap[11]=[0,0];
+    zocMap[21]=[0,0];
+    zocMap[31]=[0,0];
+    zocMap[41]=[0,0];
+    zocMap[51]=[0,0];
+    zocMap[2]=[0,0];
+    zocMap[12]=[0,0];
+    zocMap[22]=[0,0];
+    zocMap[32]=[0,0];
+    zocMap[42]=[0,0];
+    zocMap[52]=[0,0];
+    zocMap[3]=[0,0];
+    zocMap[13]=[0,0];
+    zocMap[23]=[0,0];
+    zocMap[33]=[0,0];
+    zocMap[43]=[0,0];
+    zocMap[53]=[0,0];
+    zocMap[4]=[0,0];
+    zocMap[14]=[0,0];
+    zocMap[24]=[0,0];
+    zocMap[34]=[0,0];
+    zocMap[44]=[0,0];
+    zocMap[54]=[0,0];
+    zocMap[5]=[0,0];
+    zocMap[15]=[0,0];
+    zocMap[25]=[0,0];
+    zocMap[35]=[0,0];
+    zocMap[45]=[0,0];
+    zocMap[55]=[0,0];
     var canMove;
     for(var panel_num in wkMap){
+        if(wkMap[panel_num]===0){
+            continue;   
+        }
         if(wkMap[panel_num]*turn_player>0){
             canMove=getCanMovePanelX(panel_num,wkMap,false)
         }else if (wkMap[panel_num]*turn_player<0){
@@ -197,17 +305,22 @@ function evalMap(wkMap,turn_player){
     var evMap=copyMap(wkMap);
     //終局判定
     if(isEnd(+1,evMap)){
-        return +99999;
+        return +999999;
     }else if(isEnd(-1,evMap)){
-        return -99999;
+        return -999999;
     }
-
+    if(isEndNear(+1,evMap)){
+        ev=NEARWIN_POINT;
+    }
+    if(isEndNear(-1,evMap)){
+        ev=NEARWIN_POINT*-1;
+    }
     //ZOC計算
     zocMap=getZOC(evMap,turn_player);
     
 
     //次にすぐに取られる運命のコマは死んだあつかい。
-    var deathflag=0;
+    var deathflag=false;
     for(var panel_num in evMap){
         z=zocMap[panel_num][0]-zocMap[panel_num][1];            
         if(turn_player*z<0 &&turn_player*evMap[panel_num]>0){
@@ -217,10 +330,10 @@ function evalMap(wkMap,turn_player){
                 ev+=PIECE_POINT[Math.abs(evMap[panel_num])]*-1*0.5;
             }
             evMap[panel_num]=0;
-            deathflag=1;
+            deathflag=true;
         }
     }
-    if(deathflag==1){
+    if(deathflag===true){
         zocMap=getZOC(evMap,turn_player);    
     }
     
@@ -231,12 +344,12 @@ function evalMap(wkMap,turn_player){
         var cell_p=0;
         var p=evMap[panel_num];
         var z;
-        if(turn_player==1){
+        var line;
+        if(turn_player===1){
             z=zocMap[panel_num][0]-zocMap[panel_num][1]*1.1;            
         }else{
             z=zocMap[panel_num][0]*1.1-zocMap[panel_num][1];
         }
-        var line;
         
         //コマの評価値を加算
         if(p>0){
@@ -280,7 +393,7 @@ function think(wkMap,turn_player){
     for(var i =0;i<=nodeList.length-1;i++){
         var hand=nodeList[i][0];
         var ev =evalMap(nodeList[i][1],turn_player);
-        if(best_ev==null){
+        if(best_ev===null){
             best_hand=hand[0];
             best_ev=ev;
         }
@@ -301,7 +414,7 @@ function think(wkMap,turn_player){
 function deepThinkAll(map,turn_player,depth){
 	var best_score=turn_player*9999999*-1;
 	var besthand;
-	if(depth==0){
+	if(depth===0){
 		best_score=evalMap(map,turn_player);
 		return [besthand,best_score]
 	}
@@ -317,7 +430,7 @@ function deepThinkAll(map,turn_player,depth){
         }
         //必敗
         if(isEnd(turn_player*-1,map)){
-            if(besthand==undefined){
+            if(besthand===undefined){
                 best_score=99999*turn_player*-1;
                 besthand=hand;
             }
@@ -325,7 +438,7 @@ function deepThinkAll(map,turn_player,depth){
         }
 		var sc  =deepThinkAll(map,turn_player*-1,depth-1)[1]
         
-		if(besthand==undefined){
+		if(besthand===undefined){
 			best_score=sc;
 			besthand=hand;
 		}
@@ -359,12 +472,12 @@ function deepThinkAllAB(map,turn_player,depth,a,b){
         
         //必勝
         if(isEnd(turn_player,map)){
-            return [hand,99999*turn_player];
+            return [hand,999999*turn_player];
         }
         //必敗
         if(isEnd(turn_player*-1,map)){
             if(besthand==undefined){
-                best_score=99999*turn_player*-1;
+                best_score=999999*turn_player*-1;
                 besthand=hand;
             }
             continue;
@@ -374,17 +487,17 @@ function deepThinkAllAB(map,turn_player,depth,a,b){
 			best_score=sc;
 			besthand=hand;
 		}        
-		if(turn_player==1 &&sc>best_score){
+		if(turn_player===1 &&sc>best_score){
 			best_score=sc;
 			besthand=hand;
-		}else if(turn_player==-1&&sc<best_score){
+		}else if(turn_player===-1&&sc<best_score){
 			best_score=sc;
 			besthand=hand;
 		}
-        if(turn_player==1&&a<best_score||turn_player==-1&&a>best_score){
+        if(turn_player===1&&a<best_score||turn_player===-1&&a>best_score){
             a= best_score;
         }
-        if(turn_player==1&&b<=best_score||turn_player==-1&&b>=best_score){
+        if(turn_player===1&&b<=best_score||turn_player===-1&&b>=best_score){
             break;
         }
         
