51fdd35900878fb0aff0f8d8731f02b5e90dee12
gabill <gabill@macbook.local>
2014-12-22 21:23:03 +0900

    imgのロードイベントが発火しない時のために2.5秒後に強制描画
    なぜかAndroid版ChromeでGoogle検索から飛ぶとimgイベントが発火しない。

--- boardgame.js
+++ boardgame.js
@@ -264,7 +264,13 @@ $(function(){
         img_bk_loaded=true;
         flush(true,false);
     }
-    
+    // 2.5秒後に強制描画※Googleの検索結果から飛ぶとなぜか描画が走らない事があるので。
+    setTimeout(function() {
+        if(img_bk.width!=0){
+            img_bk_loaded=true;
+        }
+        flush(true,false);
+    }, 2500);  
     updateMessage();
 
 });
