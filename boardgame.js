ab2b626315aaf30742e668bcd91027dadd078dd8
kurehajime <x.iidec+gh@googlemail.com>
2015-10-20 21:29:04 +0900

    fix iPad

--- boardgame.js
+++ boardgame.js
@@ -329,7 +329,7 @@ $(function(){
         if(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream){
             var w=screen.width;
             if(Math.abs(window.orientation) != 0){
-                   w-=35;
+                var w=screen.height;
             }
             var rate=Math.round((w/520)*1000) / 1000.0;
             if(rate==Math.round(rate)){//iOS 9のViewportは整数指定すると機能しない
