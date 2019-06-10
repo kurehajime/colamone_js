f1d66b49acd4ae13cbeeadd6f2bed93d1aaad617
kurehajime <xiidec@gmail.com>
2019-06-10 21:06:02 +0900

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,7 @@ module.exports = {
         rules: [
           {
             test: /\.js$/,
+            exclude: /node_modules/,
             use: [
               {
                 loader: "babel-loader",
