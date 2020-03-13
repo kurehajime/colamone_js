fcfaae705cac7746ba68b165343c6f794992c7b8
kurehajime <xiidec@gmail.com>
2020-03-13 22:40:11 +0900

    fix help bug

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* @license Copyright (c) @kurehajime / source code: https://github.com/kurehajime/colamone_js */
-let version = '202002260001';
+let version = '202003132239';
 self.addEventListener('install', function(event) {
   caches.keys().then(function(names) {
     for (let i in names){
