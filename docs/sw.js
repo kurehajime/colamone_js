32aefaf99223bd4619023854c12764ded787c443
kurehajime <xiidec@gmail.com>
2020-08-03 20:59:06 +0900

    update packages

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* @license Copyright (c) @kurehajime / source code: https://github.com/kurehajime/colamone_js */
-let version = '202004071959';
+let version = '202008032057';
 self.addEventListener('install', function(event) {
   caches.keys().then(function(names) {
     for (let i in names){
